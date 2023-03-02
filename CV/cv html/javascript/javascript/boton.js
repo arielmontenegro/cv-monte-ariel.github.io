84087508635012f73d140f0e4d4baaa417bd6f88
arielmontenegro <126124149+arielmontenegro@users.noreply.github.com>
2023-03-01 18:50:47 -0600

    Add files via upload

---
+++ CV/cv html/javascript/javascript/boton.js
@@ -0,0 +1,19 @@
+function abrir_afip() {
+         
+         window.open("http://www.afip.gob.ar/","AFIP","width=150,height=150,scrollbars=no")
+         }
+                      
+
+         
+function ver_IBM() {
+        window.open("https://www.ibm.com/", "IBM","width=150,height=150,scrollbars=no")
+         }
+
+
+
+function abrir_afip1() {
+         window.open("http://www.afip.gob.ar/", "Pagina_AFIP","width=150,height=150,scrollbars=no")
+         }
+         
+
+      
